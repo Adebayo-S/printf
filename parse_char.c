41bfe6733ddb809d20bff57c48c3e0f360dac35d
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-16 20:40:22 +0100

    [fix]: typecasted return value on parse_char

--- parse_char.c
+++ parse_char.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * parse_char - returns the character
+ * @c: pointer to argument
+ * Return: typecasted argument
+ */
 char parse_char(char* c) {
-
+	return ((char)*c);
 }
