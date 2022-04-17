1e6ae4c30b6dcb9decc6a0dba7ab3a3a472a01b1
Gejix <geraldjuwah@gmail.com>
2022-04-17 23:11:16 +0100

    [fix]: Updated header file

--- main.h
+++ main.h
@@ -12,8 +12,7 @@ int print_buff(char *buff, unsigned int nbuff);
 int str_len(char *s);
 char *_strcpy(char *dest, char *src);
 char parse_char(va_list arg);
-int parse_string(va_list args, char *buff, unsigned int buff_count);
-int parse_string(va_list args, char *buff, unsigned int buff_count);
-char parse_char(va_list arg);
+int parse_string(char *string, va_list arg, int buff_count);
+int parse_int(char *string, va_list list, int buff_count);
 
 #endif
