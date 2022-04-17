fb82f3e9e8e11daef34ddd905296451cafe97696
Gejix <geraldjuwah@gmail.com>
2022-04-17 22:52:44 +0100

    [bug]: Fixed bug in file

--- parse_string.c
+++ parse_string.c
@@ -4,23 +4,12 @@
  * parse_string - writes the string
  * @args: string list of arguments
  * @buff: a character string
- * @buff_count: index for buffer pointer
+ * @buff_count: index f buffer pointer
  * Return: The number of characters printed.
  */
-int parse_string(va_list args, char *buff, unsigned int buff_count)
+int parse_string(va_list arg)
 {
-    char *str;
-    unsigned int i;
-    char nill[] = "(null)";
+	int arg_s = va_arg(arg, int);
 
-    str = va_arg(args, char *);
-    if (str == NULL)
-    {
-        for (i = 0; nill[i]; i++)
-            buff_count = buff_append(buff, nill[i], buff_count);
-        return (6);
-    }
-    for (i = 0; str[i]; i++)
-        buff_count = buff_append(buff, str[i], buff_count);
-    return (i);
+	return ((char)arg_s);
 }
