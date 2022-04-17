2e3eedbc10dfde9fb77edac25dd4e86e0b5d771a
Gejix <geraldjuwah@gmail.com>
2022-04-17 19:47:46 +0100

    [feat]: Added file parse_strinf.c

---
+++ parse_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * parse_str - writes the string
+ * @args: string list of arguments
+ * @buff: a character string
+ * @buff_count: index for buffer pointer
+ * Return: The number of characters printed.
+ */
+int parse_str(va_list args, char *buff, unsigned int buff_count)
+{
+    char *str;
+    unsigned int i;
+    char nill[] = "(null)";
+
+    str = va_arg(args, char *);
+    if (str == NULL)
+    {
+        for (i = 0; nill[i]; i++)
+            buff_count = buff_append(buff, nill[i], buff_count);
+        return (6);
+    }
+    for (i = 0; str[i]; i++)
+        buff_count = buff_append(buff, str[i], buff_count);
+    return (i);
+}
