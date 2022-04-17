a58601d0431c6e9ba5eb2e16b3720b8d4f0c72a8
Gejix <geraldjuwah@gmail.com>
2022-04-17 22:58:01 +0100

    [fix]: Fixed bug in file

--- main.h
+++ main.h
@@ -11,12 +11,9 @@ char buff_append(char *buff_dest, va_list arg, unsigned int buff_count, char (*p
 int print_buff(char *buff, unsigned int nbuff);
 int str_len(char *s);
 char *_strcpy(char *dest, char *src);
-<<<<<<< HEAD
 char parse_char(va_list arg);
 int parse_string(va_list args, char *buff, unsigned int buff_count);
-=======
 int parse_string(va_list args, char *buff, unsigned int buff_count);
 char parse_char(va_list arg);
->>>>>>> fbe18450ef0034d960a67500f449feb74226cf8e
 
 #endif
