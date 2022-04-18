e404c8e57d8b2a10fd7b9fa466b22c2f85b4dbbd
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-18 01:11:51 +0100

    [fix]: restructure this code

--- buff_append.c
+++ buff_append.c
@@ -13,12 +13,18 @@ int buff_append(char *buff_dest, va_list arg, int buff_count, char type)
 	switch (type)
 	{
 		case 'c':
-				*buff_dest = parse_char(arg);
+				/*int z_c;
+				z_c = va_arg(arg, int);*/
+				buff_dest[buff_count] = 'b';
 				buff_count++;
+
+				/* *buff_dest = (char)parse_char(arg);
+				buff_count++; */
 				return (buff_count);
 				break;
 		case 's':
-				parse_string(*buff_dest, arg, buff_count);
+				parse_string(buff_dest, arg, buff_count);
 				break;
 	}
+	return (0);
 }
