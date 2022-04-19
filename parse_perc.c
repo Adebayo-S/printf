1ae48cc37cbe4cef7185410b602ca1ac7976bedc
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-19 04:07:47 +0100

    [feat]: function to parse percentage char

---
+++ parse_perc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * parse_char - returns the character
+ * @buff_dest: character
+ * @arg: list of arguments
+ * @buff_count: index of buffer pointer
+ * Return: typecasted argument
+ */
+int parse_perc(char *buff_dest, va_list arg, int buff_count)
+{
+	char c = va_arg(arg, int);
+
+	c = '%';
+
+	buff_dest[buff_count] = c;
+
+	return (++buff_count);
+}
