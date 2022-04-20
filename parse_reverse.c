03390773b0481999d6d5da6f0484a1015bf0745f
Gejix <geraldjuwah@gmail.com>
2022-04-20 22:27:43 +0100

    [feat #58]: Added file parse_reverse.c

---
+++ parse_reverse.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * parse_reverse - substitute %r by argument number in reverse
+ * @buff_dest: string to change
+ * @arg: va_list char to change
+ * @buff_count: index of dst where the c of %c is
+ * Return: New index
+ */
+int parse_reverse(char *buff_dest, va_list arg, int buff_count)
+{
+	int i = 0;
+	char *str;
+
+	str = va_arg(arg, char *);
+	if (str)
+	{
+		while (str[i] != '\0')
+			i++;
+		i = i - 1;
+		while (i >= 0)
+		{
+			buff_dest[buff_count] = str[i];
+			buff_count++;
+			i--;
+		}
+	}
+	return (buff_count);
+}
