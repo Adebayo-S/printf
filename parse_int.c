3bf24c35db0510f99151421178ec53c7115166d4
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-20 01:43:42 +0100

    [style]: comment fixed

--- parse_int.c
+++ parse_int.c
@@ -3,17 +3,17 @@
 /**
  * parse_int - substitute %i by argument number
  * @buff_dest: string to change
- * @list: va_list char to change
+ * @arg: va_list arg to change
  * @buff_count: index of dst where the c of %c is
  * Return: New index
  */
-int parse_int(char *buff_dest, va_list list, int buff_count)
+int parse_int(char *buff_dest, va_list arg, int buff_count)
 {
 	int tens = 1;
 	unsigned int tmp;
 	int number;
 
-	number = va_arg(list, int);
+	number = va_arg(arg, int);
 
 	if (number < 0)
 	{
