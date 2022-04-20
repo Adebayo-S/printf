925f6ce0a79b972c4ad9e3e3ea27ae50bb6820ae
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-20 22:46:06 +0100

    [fix]: updated header

--- main.h
+++ main.h
@@ -29,7 +29,7 @@ int parse_char(char *buff_dest, va_list arg, int buff_count);
 int parse_string(char *buff_dest, va_list arg, int buff_count);
 int parse_int(char *buff_dest, va_list list, int buff_count);
 int parse_perc(char *buff_dest, va_list arg, int buff_count);
-int parse_binary(char *buff_dest, va_list arg, int buff_count);
+int parse_bin(char *buff_dest, va_list arg, int buff_count);
 int parse_oct(char *buff_dest, va_list arg, int buff_count);
 int parse_hex(char *buff_dest, va_list arg, int buff_count);
 int parse_uint(char *buff_dest, va_list arg, int buff_count);
