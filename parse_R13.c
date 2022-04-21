51690202e1f3b2ce84580228e1fc474eab35a78c
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-21 01:32:35 +0100

    [style #59]: code cleanup

--- parse_R13.c
+++ parse_R13.c
@@ -4,30 +4,30 @@
  * parse_R13 - substitute %R by argument number in rot13
  * @buff_dest: string to change
  * @arg: va_list char to change
- * @buff_count: index of dst where the c of %c is
+ * @buff_count: index of dst where the R of %R is
  * Return: New index
  */
 int parse_R13(char *buff_dest, va_list arg, int buff_count)
 {
-    char *str;
-    char *keyset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char *keynew = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-    int key_i, carry = 0;
+	char *str;
+	char *keys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *keynew = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j = 0;
 
-    str = va_arg(arg, char *);
-    while (str[carry])
-    {
-        for (key_i = 0; keyset[key_i]; key_i++)
-        {
-            if (str[carry] == keyset[key_i])
-            {
-                buff_dest[buff_count] = keynew[key_i];
-                break;
-            }
-            buff_dest[buff_count] = str[carry];
-        }
-        buff_count++;
-        carry++;
-    }
-    return (buff_count);
+	str = va_arg(arg, char *);
+	while (str[j])
+	{
+		for (i = 0; keys[i]; i++)
+		{
+			if (str[j] == keys[i])
+			{
+				buff_dest[buff_count] = keynew[i];
+				break;
+			}
+			buff_dest[buff_count] = str[j];
+		}
+		j++, buff_count++;
+	}
+
+	return (buff_count);
 }
